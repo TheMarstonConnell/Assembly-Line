ceba38d1459fdabdf8ead26dca3b25b33e1c7b8e
TheMarstonConnell <34043723+TheMarstonConnell@users.noreply.github.com>
2019-06-05 12:20:01 -0400

    Arudino Analog Testing

--- src/com/mic/assembly/arduino/ArduinoHandler.java
+++ src/com/mic/assembly/arduino/ArduinoHandler.java
@@ -55,5 +55,28 @@ public class ArduinoHandler {
 		boolean read = uno.readBoolean();
 		return read;
 	}
+	
+	public int analogRead(int port) {
+		uno.serialWrite("3" + port);
+		while (!(uno.getSerialPort().bytesAvailable() > 0)) {
+			System.out.println("Waiting...");
+		}
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Bytes available:" + uno.getSerialPort().bytesAvailable());
+		String read = uno.serialRead();
+		if(read.substring(0, 2).equals("ok")) {
+			read = read.substring(2);
+		}
+		int i = Integer.parseInt(read);
+		if(i > 999) {
+			i = 999;
+		}
+		return i;
+	}
 
 }
